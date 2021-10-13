c9065659967836f6e75dbce69e04c4cf4dd009a8
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-10-13 11:59:24 -0300

    Agregado proyecto java base

---
+++ clases_animales/src/clases_animales/Clases_animales.java
@@ -0,0 +1,9 @@
+package clases_animales;
+
+public class Clases_animales {
+
+    public static void main(String[] args) {
+        // TODO agregar clases de animales con sus atributos por cada alumno
+    }
+    
+}
